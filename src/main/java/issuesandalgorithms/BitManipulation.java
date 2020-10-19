ce6a9e6ca94b122505182e4689b1037d077bdab2
Kamil Machul <kamil.machul@gmail.com>
2020-10-19 17:31:12 +0200

    Chapter 5. Issues and Algorithms part 2

--- src/main/java/issuesandalgorithms/BitManipulation.java
+++ src/main/java/issuesandalgorithms/BitManipulation.java
@@ -1,6 +1,7 @@
 package issuesandalgorithms;
 
 public class BitManipulation {
+    //5.1
     public static int updateBits(int n, int m, int i, int j) {
         if (i > j && Integer.toBinaryString(m).length() > (j - i)) {
             return -1;
@@ -19,5 +20,28 @@ public class BitManipulation {
         int m_shifted = m << i;
         //add them
         return n_cleared | m_shifted;
+    }
+
+    //5.2
+    public static String printBinary(double num) {
+        if (num >= 1 || num <= 0) {
+            return "ERROR1";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (num > 0) {
+            if (binary.length() >= 32) {
+                return "ERROR2";
+            }
+            double r = num * 2;
+            if (r >= 1) {
+                binary.append(1);
+                num = r - 1;
+            } else {
+                binary.append(0);
+                num = r;
+            }
+        }
+        return binary.toString();
     }
 }
--- src/test/java/BitManipulationTests.java
+++ src/test/java/BitManipulationTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 
 public class BitManipulationTests {
 
+
+    //5.1
     @Test
     public void testUpdateBits() {
         int input_n = 0b10000000000;
@@ -19,5 +21,14 @@ public class BitManipulationTests {
         int input_m = 0b10011;
         int output = BitManipulation.updateBits(input_n, input_m, 6, 2);
         assertEquals(-1, output);
+    }
+
+    //5.2
+    @Test
+    public void testPrintBinarySuccess() {
+        assertEquals("111", BitManipulation.printBinary(0.875));
+        assertEquals("11", BitManipulation.printBinary(0.75));
+        assertEquals("ERROR2", BitManipulation.printBinary(0.72));
+        assertEquals("ERROR1", BitManipulation.printBinary(1.72));
     }
 }
