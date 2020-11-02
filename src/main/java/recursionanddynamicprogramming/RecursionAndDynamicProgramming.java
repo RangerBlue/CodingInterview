0b69f83371effd04a6c21f3e78e1eacad9262a22
Kamil Machul <kamil.machul@gmail.com>
2020-11-02 21:43:41 +0100

    Chapter 9. Recursion and dynamic programming part 2

--- src/main/java/recursionanddynamicprogramming/RecursionAndDynamicProgramming.java
+++ src/main/java/recursionanddynamicprogramming/RecursionAndDynamicProgramming.java
@@ -3,46 +3,143 @@ package recursionanddynamicprogramming;
 import util.recursionanddynamicprogramic.Point;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RecursionAndDynamicProgramming {
     //9.1
-    public static int countWays(int n){
-        if(n<0 ){
+    public static int countWays(int n) {
+        if (n < 0) {
             return 0;
-        }else if(n==0){
+        } else if (n == 0) {
             return 1;
         }
-        return countWays(n-1) + countWays( n-2) +countWays(n-3);
+        return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
     }
 
-    public static int countWaysDP(int n, int[] map){
-        if(n<0 ){
+    public static int countWaysDP(int n, int[] map) {
+        if (n < 0) {
             return 0;
-        }else if(n==0){
+        } else if (n == 0) {
             return 1;
-        } else if(map[n]>-1){
-          map[n] =   countWaysDP(n-1, map) + countWaysDP( n-2, map) +countWaysDP(n-3, map);
+        } else if (map[n] > -1) {
+            map[n] = countWaysDP(n - 1, map) + countWaysDP(n - 2, map) + countWaysDP(n - 3, map);
         }
         return map[n];
     }
 
-    public static boolean getPath(int x, int y, ArrayList<Point> path){
+    public static boolean getPath(int x, int y, ArrayList<Point> path) {
         Point p = new Point(x, y);
         path.add(p);
-        if(x == 0 && y==0){
+        if (x == 0 && y == 0) {
             return true;
         }
 
         boolean success = false;
-        if(x>=1){
-            success = getPath(x-1, y, path);
+        if (x >= 1) {
+            success = getPath(x - 1, y, path);
         }
-        if(!success && y>=1){
-            success = getPath(x, y-1, path);
+        if (!success && y >= 1) {
+            success = getPath(x, y - 1, path);
         }
-        if(success){
+        if (success) {
             path.add(p);
         }
         return success;
+    }
+
+    //9.3
+    public static int findMagicIndex(int[] array, int start, int end) {
+        if (end < start || start < 0 || end >= array.length) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (array[mid] == mid) {
+            return mid;
+        } else if (array[mid] > mid) {
+            return findMagicIndex(array, start, mid - 1);
+        } else {
+            return findMagicIndex(array, mid + 1, end);
+        }
+    }
+
+    public static int findMagicIndex(int[] array) {
+        return findMagicIndex(array, 0, array.length - 1);
+    }
+
+    //9.4
+    public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index) {
+        ArrayList<ArrayList<Integer>> allsubsets;
+        if (set.size() == index) { // Przypadek bazowy - dodawanie pustego zbioru
+            allsubsets = new ArrayList<>();
+            allsubsets.add(new ArrayList<>());
+        } else {
+            allsubsets = getSubsets(set, index + 1);
+            int item = set.get(index);
+            ArrayList<ArrayList<Integer>> moresubsets = new ArrayList<>();
+            for (ArrayList<Integer> subset : allsubsets) {
+                ArrayList<Integer> newsubset = new ArrayList<>();
+                newsubset.addAll(subset);
+                newsubset.add(item);
+                moresubsets.add(newsubset);
+            }
+            allsubsets.addAll(moresubsets);
+        }
+        return allsubsets;
+    }
+
+    //9.5
+    public static ArrayList<String> getPermutations(String str) {
+        if (str == null) {
+            return null;
+        }
+        ArrayList<String> permutations = new ArrayList<>();
+        if (str.length() == 0) {
+            permutations.add("");
+            return permutations;
+        }
+
+        char first = str.charAt(0);
+        String remainder = str.substring(1);
+        ArrayList<String> words = getPermutations(remainder);
+        for (String word : words) {
+            for (int j = 0; j <= word.length(); j++) {
+                String s = insertCharAt(word, first, j);
+                permutations.add(s);
+            }
+        }
+        return permutations;
+    }
+
+    public static String insertCharAt(String word, char c, int i) {
+        String start = word.substring(0, i);
+        String end = word.substring(i);
+        return start + c + end;
+    }
+
+    //9.6
+    public static String insertInside(String str, int leftIndex) {
+        String left = str.substring(0, leftIndex + 1);
+        String right = str.substring(leftIndex + 1, str.length());
+        return left + "()" + right;
+    }
+
+    public static Set<String> generateParens(int remaining) {
+        Set<String> set = new HashSet<String>();
+        if (remaining == 0) {
+            set.add("");
+        } else {
+            Set<String> prev = generateParens(remaining - 1);
+            for (String str : prev) {
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) == '(') {
+                        String s = insertInside(str, i);
+                        set.add(s);
+                    }
+                }
+                set.add("()" + str);
+            }
+        }
+        return set;
     }
 }
--- src/test/java/RecursionAndDynamicProgrammingTests.java
+++ src/test/java/RecursionAndDynamicProgrammingTests.java
@@ -3,11 +3,15 @@ import recursionanddynamicprogramming.RecursionAndDynamicProgramming;
 import util.recursionanddynamicprogramic.Point;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 
 public class RecursionAndDynamicProgrammingTests {
+    //9.1
     @Test
     public void testCountWays() {
         assertEquals(53798080, RecursionAndDynamicProgramming.countWays(30));
@@ -30,5 +34,83 @@ public class RecursionAndDynamicProgrammingTests {
         assertEquals(5, list.size());
     }
 
+    //9.3
+    @Test
+    public void testFindMagicIndexSuccess() {
+        int[] input = {-10, -5, 0, 1, 2, 4, 6, 8, 11};
+        assertEquals(6, RecursionAndDynamicProgramming.findMagicIndex(input));
+    }
+
+    @Test
+    public void testFindMagicIndexFailure() {
+        int[] input = {-10, -5, 0, 1, 2, 4, 7, 8, 11};
+        assertEquals(-1, RecursionAndDynamicProgramming.findMagicIndex(input));
+    }
+
+    //9.4
+    @Test
+    public void testGetSubsets() {
+        ArrayList<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
+        input.add(3);
+        ArrayList<Integer> output0 = new ArrayList<>();
+        ArrayList<Integer> output1_1 = new ArrayList<>();
+        output1_1.add(1);
+        ArrayList<Integer> output1_2 = new ArrayList<>();
+        output1_2.add(2);
+        ArrayList<Integer> output1_3 = new ArrayList<>();
+        output1_3.add(3);
+        ArrayList<Integer> output2_3_2 = new ArrayList<>();
+        output2_3_2.add(3);
+        output2_3_2.add(2);
+        ArrayList<Integer> output2_3_1 = new ArrayList<>();
+        output2_3_1.add(3);
+        output2_3_1.add(1);
+        ArrayList<Integer> output2_2_1 = new ArrayList<>();
+        output2_2_1.add(2);
+        output2_2_1.add(1);
+        ArrayList<Integer> output3_3_2_1 = new ArrayList<>();
+        output3_3_2_1.add(3);
+        output3_3_2_1.add(2);
+        output3_3_2_1.add(1);
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add(output0);
+        expected.add(output1_3);
+        expected.add(output1_2);
+        expected.add(output2_3_2);
+        expected.add(output1_1);
+        expected.add(output2_3_1);
+        expected.add(output2_2_1);
+        expected.add(output3_3_2_1);
+
+        assertEquals(expected, RecursionAndDynamicProgramming.getSubsets(input, 0));
+    }
+
+    //9.5
+    @Test
+    public void testGetPermutations() {
+        String input = "ABC";
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("ABC");
+        expected.add("BAC");
+        expected.add("BCA");
+        expected.add("ACB");
+        expected.add("CAB");
+        expected.add("CBA");
+        assertEquals(expected, RecursionAndDynamicProgramming.getPermutations(input));
+    }
+
+    //9.6
+    @Test
+    public void testGetParens() {
+        Set<String> expected = new HashSet<>();
+        expected.add("()()()");
+        expected.add("()(())");
+        expected.add("(()())");
+        expected.add("(())()");
+        expected.add("((()))");
+        assertEquals(expected, RecursionAndDynamicProgramming.generateParens(3));
+    }
 }
 
